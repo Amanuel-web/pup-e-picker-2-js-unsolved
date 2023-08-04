902a0347517b34acdae07ea273352efc9bf872cc
jjhiggz <jonathan.higger@gmail.com>
2023-08-04 16:01:48 -0600

    first commit

---
+++ src/Components/Section.jsx
@@ -0,0 +1,39 @@
+export const Section = ({ label, children }) => {
+  return (
+    <section id="main-section">
+      <div className="container-header">
+        <div className="container-label">{label}</div>
+        <div className="selectors">
+          {/* This should display the favorited count */}
+          <div
+            className={`selector ${"active"}`}
+            onClick={() => {
+              alert("click favorited");
+            }}
+          >
+            favorited ( {0} )
+          </div>
+
+          {/* This should display the unfavorited count */}
+          <div
+            className={`selector ${""}`}
+            onClick={() => {
+              alert("click unfavorited");
+            }}
+          >
+            unfavorited ( {10} )
+          </div>
+          <div
+            className={`selector ${""}`}
+            onClick={() => {
+              alert("clicked create dog");
+            }}
+          >
+            create dog
+          </div>
+        </div>
+      </div>
+      <div className="content-container">{children}</div>
+    </section>
+  );
+};
